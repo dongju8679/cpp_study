de066ffb990830906f01083f138904de320af88a
dongju3 <dongju8679@gmail.com>
2020-08-19 16:18:49 +0900

    add changes

--- cpp_study/main.cpp
+++ cpp_study/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
 
 int main() {
 	int ret = 0;
+
+	map<string, int> m;
+
+	m.insert(make_pair("a", 1));
+	m.insert(make_pair("b", 2));
+	m.insert(make_pair("c", 3));
+	m.insert(make_pair("d", 4));
+	m.insert(make_pair("e", 5));
 
 	std::cout << "start cpp_study" << std::endl;
 
